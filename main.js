6a064db7aabd8f29bb0aea0ebc01c23be3768062
yeo0w0 <yeo0w0o3010@gmail.com>
2026-03-17 12:30:47 +0900

    v1.0

---
+++ main.js
@@ -0,0 +1,34 @@
+document.getElementById('time').innerHTML = new Date().toLocaleString()
+
+function pink() {
+  document.body.style.backgroundColor = 'pink'
+}
+function lightblue() {
+  document.body.style.backgroundColor = 'lightblue'
+}
+function lightyellow() {
+  document.body.style.backgroundColor = 'lightyellow'
+}
+function reset() {
+  document.body.style.backgroundColor = 'linen'
+}
+
+function showhtml() {
+  document.getElementById('fig').src = 'img/Html5.png'
+  document.getElementById('desc').innerHTML =
+    '<b>HTML</b>은 HyperText Markup Language의 약자로 웹 페이지를 만들 때 사용하는 언어입니다.'
+}
+function showcss() {
+  document.getElementById('fig').src = 'img/Css3.png'
+  document.getElementById('desc').innerHTML =
+    '<b>CSS</b>는 Cascading Style Sheets의 약자로 웹 페이지의 스타일을 지정할 때 사용하는 언어입니다.'
+}
+function showjs() {
+  document.getElementById('fig').src = 'img/JS.png'
+  document.getElementById('desc').innerHTML =
+    '<b>JavaScript</b>는 웹 페이지에 동적 기능을 추가할 때 사용하는 프로그래밍 언어입니다.'
+}
+function hide() {
+  document.getElementById('fig').src = ''
+  document.getElementById('desc').innerHTML = ''
+}
