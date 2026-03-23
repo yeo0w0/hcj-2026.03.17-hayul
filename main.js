73255da272a44639f9542a6756c2836bb9348c50
yeo0w0 <yeo0w0o3010@gmail.com>
2026-03-23 17:36:47 +0900

    update

--- main.js
+++ main.js
@@ -14,17 +14,17 @@ function reset() {
 }
 
 function showhtml() {
-  document.getElementById('fig').src = 'img/Html5.png'
+  document.getElementById('fig').src = 'Html5.png'
   document.getElementById('desc').innerHTML =
     '<b>HTML</b>은 HyperText Markup Language의 약자로 웹 페이지를 만들 때 사용하는 언어입니다.'
 }
 function showcss() {
-  document.getElementById('fig').src = 'img/Css3.png'
+  document.getElementById('fig').src = 'Css3.png'
   document.getElementById('desc').innerHTML =
     '<b>CSS</b>는 Cascading Style Sheets의 약자로 웹 페이지의 스타일을 지정할 때 사용하는 언어입니다.'
 }
 function showjs() {
-  document.getElementById('fig').src = 'img/JS.png'
+  document.getElementById('fig').src = 'JS.png'
   document.getElementById('desc').innerHTML =
     '<b>JavaScript</b>는 웹 페이지에 동적 기능을 추가할 때 사용하는 프로그래밍 언어입니다.'
 }
